1e0f82d908c3b885d8f7f02c7ba8ac41a75828f0
Vladimir Kozhin <hello@kozhindev.com>
2020-11-23 10:38:26 +0700

    add payment orders

---
+++ billing/paymentOrders.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import {generateCrud} from '@steroidsjs/core/ui/crud';
+import {MoneyFormatter} from '@steroidsjs/core/ui/format';
+import AuthLogins from '../auth/AuthLogins';
+import {getCrudGridId, ICrudClickProps} from '../../react/ui/crud/Crud/Crud';
+import {listRefresh} from '../../react/actions/list';
+
+export const ROUTE_PAYMENT_ORDERS = 'payment_orders';
+
+export default generateCrud(ROUTE_PAYMENT_ORDERS, {
+    label: __('Платежный шлюз'),
+    model: 'steroids.payment.models.PaymentOrder',
+    enums: [
+        'steroids.payment.enums.PaymentStatus',
+    ],
+    searchModel: 'steroids.payment.forms.PaymentOrdersSearch',
+    path: '/admin/billing/payment-orders',
+    restUrl: '/api/v1/admin/payment/orders',
+    create: false,
+    update: false,
+    delete: false,
+    view: false,
+    grid: {
+        searchForm: {
+            layout: 'table',
+            fields: [
+                {
+                    attribute: 'id',
+                    placeholder: __('ИД'),
+                    inputProps: {
+                        style: {
+                            width: 50
+                        }
+                    }
+                },
+                {
+                    attribute: 'payerUserQuery',
+                    placeholder: __('Email или телефон')
+                },
+                {
+                    attribute: 'externalId',
+                    placeholder: __('Внешний ИД')
+                },
+                {
+                    attribute: 'status',
+                    placeholder: __('Статус')
+                },
+            ],
+        },
+        columns: [
+            'id',
+            {
+                attribute: 'payerUserQuery',
+                label: __('Плательщик'),
+                valueView: ({item}) => (
+                    <div>
+                        {item.payerUser.name}
+                    </div>
+                ),
+            },
+            'description',
+            {
+                attribute: 'inAmount',
+                label: __('Сумма (сайт)'),
+                valueView: ({item}) => (
+                    <MoneyFormatter
+                        value={item.inAmount}
+                        currency={item.inCurrencyCode}
+                    />
+                ),
+            },
+            {
+                attribute: 'outAmount',
+                label: __('Сумма (шлюз)'),
+                valueView: ({item}) => (
+                    <MoneyFormatter
+                        value={item.outAmount}
+                        currency={item.outCurrencyCode}
+                    />
+                ),
+            },
+            'status',
+            {
+                attribute: 'method.title',
+                label: __('Метод'),
+            },
+            'externalId',
+            'createTime',
+            'updateTime',
+        ],
+    },
+    items: {
+        accept: {
+            label: __('Принять'),
+            icon: 'accept',
+            visible: (item: any) => item.status === 'process',
+            confirm: (e, id) => __('Принять ордер {id}?', {id}),
+            onClick: async (e, itemId, item, props: ICrudClickProps) => {
+                e.preventDefault();
+                await props.http.post(`/api/v1/admin/payment/orders/${itemId}/accept`);
+                props.dispatch(listRefresh(getCrudGridId(props)));
+            },
+        },
+        reject: {
+            label: __('Отклонить'),
+            icon: 'reject',
+            visible: (item: any) => item.status === 'process',
+            confirm: (e, id) => __('Отклонить ордер {id}?', {id}),
+            onClick: async (e, itemId, item, props: ICrudClickProps) => {
+                e.preventDefault();
+                await props.http.post(`/api/v1/admin/payment/orders/${itemId}/reject`);
+                props.dispatch(listRefresh(getCrudGridId(props)));
+            },
+        },
+    }
+});
